3e09e2197cac03c2ef2a979bc3a9e3e27156103c
StikyPiston <StikyPiston@codeberg.org>
2025-11-11 13:36:51 +0200

    Install lightdm

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,8 +7,11 @@ dnf5 -y copr enable solopasha/hyprland
 
 dnf5 install -y tmux ruby gcc cmake
 dnf5 install -y libavcodec-freeworld --allowerasing
-dnf5 install -y niri dms
+dnf5 install -y niri 
+dnf5 install -y swww waybar swaylock
+dnf5 install -y lightdm lightdm-settings
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+systemctl enable lightdm.service
 systemctl enable podman.socket
