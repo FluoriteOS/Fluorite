113221f880cc21f0d809adbad00cc79ffa2999e1
StikyPiston <StikyPiston@codeberg.org>
2025-11-11 13:30:53 +0200

    Install some default packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,13 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y copr enable solopasha/hyprland
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 install -y tmux ruby gcc cmake
+dnf5 install -y libavcodec-freeworld --allowerasing
+dnf5 install -y niri dms
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 systemctl enable podman.socket
