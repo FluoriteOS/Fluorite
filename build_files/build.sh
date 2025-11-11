ccf2099a3749c8e97130d5003516724287537a02
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-11-11 17:22:57 +0200

    Try and just install bazaar

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,6 +21,6 @@ dnf5 install -y croc obs-studio qt5ct qt6ct swaync fuzzel
 # Flatpak packages
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# flatpak install -y flathub menu.kando.Kando io.github.kolunmi.Bazaar com.github.marhkb.Pods
+flatpak install -y flathub io.github.kolunmi.Bazaar # com.github.marhkb.Pods menu.kando.Kando
 systemctl enable lightdm.service
 systemctl enable podman.socket
