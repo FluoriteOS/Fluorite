2c97de1e00d0a04098845a04ac01bd310853147e
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-11-12 17:54:43 +0200

    Use different copr for fonts

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ dnf5 -y copr enable avengemedia/dms
 dnf5 -y copr enable solopasha/hyprland
 dnf5 -y copr enable scottames/ghostty
 dnf5 -y copr enable atim/starship
-dnf5 -y copr enable maveonair/jetbrains-mono-nerd-fonts
+dnf5 -y copr enable harryjph/fonts
 
 dnf5 install -y tmux ruby gcc cmake
 dnf5 install -y niri dms wlogout hyprlock
@@ -19,7 +19,7 @@ dnf5 install -y ghostty
 dnf5 install -y starship
 dnf5 install -y croc qt5ct qt6ct swaync fuzzel
 dnf5 install -y pavucontrol 
-dnf5 install -y jetbrains-mono-nerd-fonts jetbrains-mononl-nerd-fonts
+dnf5 install -y jetbrains-mono-nerd-fonts
 
 # Flatpak packages
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
